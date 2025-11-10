b943bf15042dd164b1cf61ce19dab9f750dfeeda
yashsh13 <yashpat1302@gmail.com>
2025-11-10 15:18:41 +0530

    set up the basic frontend template for the main feed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import SigninPage from './pages/SigninPage'
+import MainFeedPage from './pages/MainFeedPage'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signin' element={<SigninPage />} />
+        <Route path='/feed' element={<MainFeedPage />} />
       </Routes>
     </BrowserRouter>
   )
