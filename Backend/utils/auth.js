b3151caf7d22123d9fac3d4bd1bd6a11c341ddcb
yashsh13 <yashpat1302@gmail.com>
2025-11-15 20:03:51 +0530

    Added Authentication Middleware

---
+++ Backend/utils/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const auth = (req,res,next) => {
+
+    try{
+        const token = req.headers.token;
+
+        const decoded = jwt.verify(token,process.env.JWT_PASSWORD);
+
+        req.id = decoded.userID;
+
+        next();
+
+    } catch (error) {
+        
+        return res.json({
+            message:"Authentication Middleware Error: "+error,
+            success:false
+        })
+    }
+
+}
+
+export default auth;
